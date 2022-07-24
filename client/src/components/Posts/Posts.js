943ce488429c9f491289f10f45aaa0380f76a882
علی خداوردی <alikhodaveerdi@yahoo.com>
2022-07-24 18:19:10 +0430

    jsx structure

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Post from './Post/Post';
+
+const Posts = () => {
+    return (
+        <>
+            <h1>
+                Posts
+                <Post />
+                <Post />
+            </h1>
+        </>
+    )
+}
+
+export default Posts;
